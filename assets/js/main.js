176ff2da1e04cb2069c042e8f985c583788fce49
kaderurmi <kaderurmi@gmail.com>
2024-03-18 23:58:46 +0600

    first commit JS

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* var eggPrice = 5;
+var numOfEggs = 20;
+var totalCosting = eggPrice * numOfEggs;
+console.log("Total Cost Is: "+totalCosting+ " Taka");
+
+// area of circle calculator = pi * radius square
+var radius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi * (radius *radius);
+console.log("Area of circle: " + areaOfCircle + " Sq Inch");
+
+x++// x=x=1, 
+X--// x=X-1 */
+var personOne ="Jamal";
+var personTwo = "Kamal";
+var areTheyBrother = false;
+
+if("Jamal" == personOne && "Kamal" == personTwo && areTheyBrother){
+    console.log("They are Brother");
+} else{
+    console.log("They are not Brother");
+}
