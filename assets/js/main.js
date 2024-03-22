e9c3aaa7615fd58e16a1587556e08642b7cd0f2f
Md Fazlul Kader <kaderurmi@gmail.com>
2024-03-23 01:47:21 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,13 @@ X--// x=X-1 */
 // } else{
 //     console.log("They are not Brother");
 // }
+//================
+// var n = 21;
+// if(n<10 && (2 == n || 3 == n || 5 ==n || 7 == n) ){
+//     console.log("This Number "+n+" is smaller than 10 and it's a prime number.");
+// } else if (n <10){
+//     console.log("This Number "+n+" is smaller than 10 and it's Not a prime number.");
+// }else {
+//     console.log("This Number "+n+" is greater than 10.");
+// }
 
-var n = 21;
-if(n<10 && (2 == n || 3 == n || 5 ==n || 7 == n) ){
-    console.log("This Number "+n+" is smaller than 10 and it's a prime number.");
-} else if (n <10){
-    console.log("This Number "+n+" is smaller than 10 and it's Not a prime number.");
-}else {
-    console.log("This Number "+n+" is greater than 10.");
-}
